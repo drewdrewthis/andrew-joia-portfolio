fbd696be043519c167fb8f9123b193f9af8efad2
Andrew Joia <drewdrewthis@gmail.com>
2015-11-24 18:38:45 -0500

    Adds tech specs to each port item

--- js/script.js
+++ js/script.js
@@ -3,73 +3,85 @@ $(function(){
 	var portfolio = [
 		{ 
 			title: "About Me Page",
-			tech: "HTML/CSS/JS, Design, Photoshop",
+			tech: "HTML/CSS, Design, Photoshop",
 			url: "http://codepen.io/drewdrewthis/full/BozPGe/",
 			alt: "About Me",
 			data_target: "",
-			img: "./images/aboutme-screenshot.jpg"
+			img: "./images/aboutme-screenshot.jpg",
+			show: true
 		},
 		{ 
 			title: "Who Said? Quiz Game",
-			url: "http://drewdrewthis.github.io/fewd-who-said-quiz/",
-			alt: "Who Said? Quiz",
-			data_target: "",
-			img: "./images/quiz-screenshot.jpg"
-		},
-		{ 
-			title: "Shopping List App",
-			url: "http://drewdrewthis.github.io/fewd-shopping-list/",
-			alt: "Shopping List App",
-			data_target: "",
-			img: "./images/shopping-list-screenshot.jpg"
-		},
-		{ 
-			title: "YouTube API Project",
-			url: "http://drewdrewthis.github.io/Thinkful-Tube-Project/",
-			alt: "YouTube API Project",
-			data_target: "",
-			img: "./images/thinkful-tube-screenshot.jpg"
-		},
-		{ 
-			title: "Hot or Cold Game",
-			url: "http://drewdrewthis.github.io/fewd-hot-or-cold/",
-			alt: "Hot or Cold Game",
-			data_target: "",
-			img: "./images/hot-or-cold-screenshot.jpg"
-		},
-		{ 
-			title: "Who Said? Quiz Game",
+			tech: "HTML/CSS/JS, jQuery, jQuery-UI, JSON, Design",
 			url: "http://drewdrewthis.github.io/fewd-who-said-quiz/",
 			alt: "Who Said? Quiz",
 			data_target: "",
 			img: "./images/quiz-screenshot.jpg",
+			show: true
+		},
+		{ 
+			title: "Shopping List App",
+			tech: "HTML/CSS/JS, jQuery, Design",
+			url: "http://drewdrewthis.github.io/fewd-shopping-list/",
+			alt: "Shopping List App",
+			data_target: "",
+			img: "./images/shopping-list-screenshot.jpg",
+			show: true
+		},
+		{ 
+			title: "YouTube API Project",
+			tech: "HTML/CSS/JS, jQuery, YouTube API",
+			url: "http://drewdrewthis.github.io/Thinkful-Tube-Project/",
+			alt: "YouTube API Project",
+			data_target: "",
+			img: "./images/thinkful-tube-screenshot.jpg",
+			show: true
+		},
+		{ 
+			title: "Hot or Cold Game",
+			tech: "JavaScript, jQuery",
+			url: "http://drewdrewthis.github.io/fewd-hot-or-cold/",
+			alt: "Hot or Cold Game",
+			data_target: "",
+			img: "./images/hot-or-cold-screenshot.jpg",
+			show: true
+		},
+		{
 			title: "New York Times Hack",
+			tech: "Thinkful DOM Manipulation Project",
 			url: "",
 			alt: "NYT Screenshot",
 			data_target: "#NYTScreenshot",
-			img: "./images/nyt-screenshot.jpg"
+			img: "./images/nyt-screenshot.jpg",
+			show: true
 		},
 		{ 
 			title: "Karma Clone",
+			tech: "Thinkful Clone of Existing Website HTML/CSS",
 			url: "http://codepen.io/drewdrewthis/full/BozPGe/",
 			alt: "Karma Clone",
 			data_target: "",
 			img: "./images/shopping-list-screenshot.jpg",
-			img: "./images/karma-clone-screenshot.jpg"
+			img: "./images/karma-clone-screenshot.jpg",
+			show: true
 		},
 		{ 
 			title: "Streetfighter Game",
+			tech: "JavaScript",
 			url: "http://drewdrewthis.github.io/jquery-streetfighter/main.html",
 			alt: "Streetfighter Game",
 			data_target: "",
-			img: "./images/streetfighter-screenshot.jpg"
+			img: "./images/streetfighter-screenshot.jpg",
+			show: true
 		},
 		{ 
 			title: "Shh.. <em>FizzBuzz</em>",
+			tech: "HTML, JavaScript",
 			url: "",
 			alt: "FizzBuzz",
 			data_target: "#FizzBuzz",
-			img: "./images/fizzbuzz.jpg"
+			img: "./images/fizzbuzz.jpg",
+			show: true
 		}
 	]
 
@@ -82,7 +94,7 @@ $(function(){
 				portItem.data_target + '" style="background-image:url(' +
 				portItem.img + ');" target="_blank">\
 					<div class="fig-caption"><span>' + portItem.title + 
-						'</span><p class="fig-tech">' + portItem.tech + 
+						'</span><p class="fig-tech">' + (portItem.tech || "")+ 
 						'</p>\
 					</div>\
 				</a>';
